f2f48157d0a2f08251dc2dd920a97378c373909c
tymur1404 <t-aze@yandex.ru>
2016-11-07 17:27:57 +0200

    Update View.php

--- app/code/local/Ainstainer/TechTalk/Block/View.php
+++ app/code/local/Ainstainer/TechTalk/Block/View.php
@@ -12,10 +12,11 @@ class Ainstainer_TechTalk_Block_View extends Mage_Core_Block_Template
         return Mage::getModel('techtalk/contact')->load(1); 
     }
 
-    public function getOtherComments($id)// get all comments except one
+    public function getOtherComments($id)// get all approved comments except one
     {
-        return Mage::getModel('techtalk/contact')->getCollection()->
-        addFieldToFilter('request_id',array("neq" => $id));
+        return Mage::getModel('techtalk/contact')->getCollection()
+                    ->addFieldToFilter('request_id',array("neq" => $id))
+                    ->addFieldToFilter('approved',array("eq" => 1));// 1 - TRUE, 0 - FALSE
     }
 
 }
